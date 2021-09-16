8aa319a54d375cb59413a68414e04d1b8e1227ce
UpekshaHerath <upekshadilshan000@gmail.com>
2021-09-15 19:03:30 -0700

    changed the js file

--- controll.js
+++ controll.js
@@ -1,8 +1,6 @@
-console.log('This is javaScript');
-
 let textField = document.querySelector('.inputField');
 let submit = document.querySelector('.submit');
-let previous_guess = document.querySelector('.previous_guess');
+let previous_guesses = document.querySelector('.previous_guess');
 let results = document.querySelector('.results');
 let newGame = document.querySelector('.newGame');
 
@@ -13,18 +11,30 @@ submit.onclick = function() {
     work_done();
 }
 
+let guesses = 0;
+let lastGuesses = 'The previous guesses are : ';
 function work_done() {
+   
     let value = Number(textField.value);
-        if(value == randomNumber) {
-            results.textContent = 'You won the game !!';
-        }
-        if(value > randomNumber) {
-            results.textContent = 'Value is larger than guessing number';
-        }
-        if(value < randomNumber) {
-            results.textContent = 'Value is smaller than guessing number';
-        }
-    
+    lastGuesses += value + " ";
+    previous_guesses.textContent = lastGuesses;
+    if(value == randomNumber) {
+        results.textContent = 'You won the game !!';
+        textField.value = "";
+    }
+    if(value > randomNumber) {
+        results.textContent = 'Value is larger than guessing number';
+        textField.value = "";
+    }
+    if(value < randomNumber) {
+        results.textContent = 'Value is smaller than guessing number';
+        textField.value = "";
+    }
+    guesses++;
+    if(guesses == 5){
+        results.textContent = 'Your number of guesses are over. Start a new game';
+    }
+        
         
    
     
